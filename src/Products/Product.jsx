4329ad94853963705eba5bb52255bad1f8fefa97
aswath0001 <rsaswaths38@gmail.com>
2025-03-10 22:43:29 +0530

    StepUp

--- src/Products/Product.jsx
+++ src/Products/Product.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
-import './Product.css'
+import React from 'react';
+import './Product.css';
+import { IoStarSharp,IoBag } from "react-icons/io5";
 const Product = () => {
   return (
-    <div>Product</div>
+    <>
+    <section className='card-container'>
+     <section className='card'>
+      <img className='card-img'  src='https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg'
+      alt='Shoe'/>
+      <div className="card-details">
+        <h3 className='card-title'>Shoe</h3>
+        <section className="card-reviews">
+          <span className="total-reviews">4</span>
+         <IoStarSharp className='rating-star'/>
+         <IoStarSharp className='rating-star'/>
+         <IoStarSharp className='rating-star'/>
+         <IoStarSharp className='rating-star'/>
+        </section>
+        <section className="card-price">
+          <div className='price'>
+            <del>$299</del>199
+          </div>
+          <div className="bag">
+          <IoBag />
+          </div>
+        </section>
+      </div>
+     </section>
+    </section>
+    </>
   )
 }
 
